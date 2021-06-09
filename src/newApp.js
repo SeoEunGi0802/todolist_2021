9c47ea98350192f36c13462fcfb742689e9e5049
SeoEunGi0802 <lolove08@naver.com>
2021-06-09 12:30:07 +0900

    0609 업로드

--- src/newApp.js
+++ src/newApp.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Typography } from '@material-ui/core';
-import './App.css';
 import InputArea from './components/InputArea'
 import ListArea from './components/ListArea'
+import './App.css';
 
 function NewApp(props) {
     const [todoList, setTodoList] = useState([]);
